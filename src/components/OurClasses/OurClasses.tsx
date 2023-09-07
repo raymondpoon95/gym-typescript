1e73956df16f866e7762e73dee99301caa5996a3
Raymond Poon <raymondpoon95@hotmail.co.uk>
2023-09-07 14:07:54 +0100

    created ourClasses component

---
+++ src/components/OurClasses/OurClasses.tsx
@@ -0,0 +1,11 @@
+import { SelectedPage } from "../../common/types";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return <div>OurClasses</div>;
+};
+
+export default OurClasses;
